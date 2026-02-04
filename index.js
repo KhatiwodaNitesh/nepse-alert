cf923f3b2c4d57b7007d5496375964c16971724e
Nitesh Khatiwoda <57801169+KhatiwodaNitesh@users.noreply.github.com>
2026-02-04 21:41:25 +0545

    U

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Nepse Alert JS script started");
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+if (!BOT_TOKEN || !CHAT_ID) {
+  console.error("Missing Telegram secrets");
+  process.exit(1);
+}
+
+const message = "✅ GitHub Actions successfully sent this message!";
+
+const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+async function sendMessage() {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: message,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (!data.ok) {
+    console.error("Telegram API error:", data);
+    process.exit(1);
+  }
+
+  console.log("Message sent successfully");
+}
+
+sendMessage();
